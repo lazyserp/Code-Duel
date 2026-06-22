643ab6b6b46ae459b36e59f99dfbf6df155c1456
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 00:34:43 +0530

    Added UserRepository and its methods

---
+++ src/main/java/com/codeduel/codeduel/auth/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.codeduel.codeduel.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codeduel.codeduel.auth.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User,UUID>{
+
+    public Optional<User> findByEmail(String email);
+    public Optional<User> findByUsername(String username);
+
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+    
+}
