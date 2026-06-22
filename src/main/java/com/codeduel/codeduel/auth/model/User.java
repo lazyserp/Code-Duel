03ae7c8f7b0089b185659aa19816ed3a8505595b
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 00:21:18 +0530

    feat : Added the User model

---
+++ src/main/java/com/codeduel/codeduel/auth/model/User.java
@@ -0,0 +1,44 @@
+package com.codeduel.codeduel.auth.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+    private boolean isOnline;
+
+    @Builder.Default
+    private int currentElo = 1200; // ELO Ranking system
+
+    
+}
