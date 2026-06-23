30e62f9d7ac6113081502f5897482fb7888b533e
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 11:01:21 +0530

    Custom check for superDetails using interface

---
+++ src/main/java/com/codeduel/codeduel/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.codeduel.codeduel.auth.service;
+
+import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.codeduel.codeduel.auth.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String username)
+    {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+    }
+
+
+}
