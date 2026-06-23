1121588d6e7deaf28bc38618352fea01e1a86ae2
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 14:23:30 +0530

    Created the Auth Route

---
+++ src/main/java/com/codeduel/codeduel/auth/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.codeduel.codeduel.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codeduel.codeduel.auth.dto.AuthResponse;
+import com.codeduel.codeduel.auth.dto.LoginRequest;
+import com.codeduel.codeduel.auth.dto.RegisterRequest;
+import com.codeduel.codeduel.auth.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) 
+    {
+        AuthResponse res = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    {
+        AuthResponse res = authService.login(request);
+
+       return  ResponseEntity.ok(res);
+
+    }
+
+
+
+    
+}
