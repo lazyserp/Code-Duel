327f2d29383ceaf20d10e8917d56b61f66212a59
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 23:39:17 +0530

    feat : Added validation for Not Blank fields

--- src/main/java/com/codeduel/codeduel/auth/dto/RegisterRequest.java
+++ src/main/java/com/codeduel/codeduel/auth/dto/RegisterRequest.java
@@ -1,5 +1,9 @@
 package com.codeduel.codeduel.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterRequest(@NotBlank String username,@NotBlank String email ,@NotBlank String password) {};
+public record RegisterRequest(
+    @NotBlank String username,
+    @NotBlank @Email String email ,
+    @NotBlank String password) {};
