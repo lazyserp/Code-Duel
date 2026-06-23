148c3dad5e6be5844c38ffba76d247d7a84ea409
Aman Jha <Aman.Jha@wissen.com>
2026-06-23 23:38:37 +0530

    feat : Added email validator

--- src/main/java/com/codeduel/codeduel/auth/dto/LoginRequest.java
+++ src/main/java/com/codeduel/codeduel/auth/dto/LoginRequest.java
@@ -1,3 +1,5 @@
 package com.codeduel.codeduel.auth.dto;
 
-public record LoginRequest(String username , String password) {};
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(@NotBlank String username , @NotBlank String password) {};
