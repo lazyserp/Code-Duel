6b9cb7bb6f2a984307603afb7674c0bf235b3569
Aman Jha <Aman.Jha@wissen.com>
2026-06-25 14:32:07 +0530

    feat : Added V2 DB migration with problem and match tables

---
+++ src/main/resources/db/migration/V2__create_problems_and_matches_tables.sql
@@ -0,0 +1,28 @@
+CREATE TYPE difficulty_enum AS ENUM (
+    'EASY',
+    'MEDIUM',
+    'HARD'
+);
+
+CREATE TABLE problems(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    difficulty difficulty_enum NOT NULL,
+    test_cases JSONB,
+    starter_code JSONB
+);
+
+CREATE TABLE matches(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    problem_id UUID REFERENCES problems,
+    user1_id UUID REFERENCES users,
+    user2_id UUID REFERENCES users,
+    winner_id UUID REFERENCES users,
+    status VARCHAR DEFAULT 'QUEUED',
+    started_at TIMESTAMP,
+    ended_at TIMESTAMP
+);
+
+
+
