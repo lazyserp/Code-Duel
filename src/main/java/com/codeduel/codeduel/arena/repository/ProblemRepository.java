7f46304aa216785966215bb1c2b06c61d250b639
Aman Jha <Aman.Jha@wissen.com>
2026-06-26 13:50:06 +0530

    feat : Added findByDifficulty mehtod

--- src/main/java/com/codeduel/codeduel/arena/repository/ProblemRepository.java
+++ src/main/java/com/codeduel/codeduel/arena/repository/ProblemRepository.java
@@ -3,6 +3,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeduel.codeduel.arena.model.Problem;
 import java.util.UUID;
 
+import com.codeduel.codeduel.arena.model.Difficulty;
+import java.util.List;
+
 public interface ProblemRepository extends JpaRepository<Problem,UUID>{
-    
+    List<Problem> findByDifficulty(Difficulty difficulty);
 }
